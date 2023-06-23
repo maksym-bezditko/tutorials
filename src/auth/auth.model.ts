4f621a2c4eb81f7141c13829365e07fc1c640a77
Maksym Bezditko <maksym.bezditko@develops.today>
2023-06-24 02:51:32 +0300

    refactor(app): remove redundant comment

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,7 +1,6 @@
 import { prop } from '@typegoose/typegoose';
 import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface AuthModel extends Base {}
 export class AuthModel extends TimeStamps {
   @prop({ unique: true })
